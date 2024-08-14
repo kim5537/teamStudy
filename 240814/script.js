da4e56d64ec7991b61cefec69fb6fde6eb6f6176
kim5537 <fudgml778@naver.com>
2024-08-14 17:25:02 +0900

    240814

---
+++ 240814/script.js
@@ -0,0 +1,10 @@
+const num1 = 10;
+const num2 = 7;
+
+const n01 = num1 + num2;
+const n02 = num1 - num2;
+const n03 = num1 * num2;
+const n04 = (num1 / num2).toFixed(2);
+const n05 = num1 % num2;
+
+console.log(`${n01},${n02},${n03},${n04},${n05}`);
